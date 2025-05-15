f2f81ccfcf574bfd7c9e4bb093680b3ad28b7733
Jteccomputer <chukwukachinedujohn@gmail.com>
2025-05-15 12:01:42 +0100

    Saved data and generated HTML fro delivery options.

--- data/cart.js
+++ data/cart.js
@@ -4,11 +4,14 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if(!cart) {
       cart = [{
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2
+      quantity: 2,
+      deliveryOptionId: '1' 
    }, {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1
+      quantity: 1,
+      deliveryOptionId: '2'
    }];
+   
 };
 
 // function that save our cart to localStorage
@@ -30,8 +33,8 @@ export function addToCart(productId) {
          } else {
             cart.push({
             productId: productId,
-            quantity: 1
-
+            quantity: 1,
+            deliveryOptionId: '1'
           });
 
          }     
@@ -42,12 +45,15 @@ export function removeFromCart(productId) {
    // step 1, create a new empty array
    const newCart = [];
 
+   // Loop through the array, and push 
+   // the ones did not match into the new array
    cart.forEach((cartItem) => {
        if(cartItem.productId !== productId) {
          newCart.push(cartItem);
        }
    });
 
+   // Pass the newCart into the cart
    cart = newCart;
 
    saveToStorage();
