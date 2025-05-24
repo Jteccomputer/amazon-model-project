c78f95d435b2639a57ef994fcae4f12220e3cded
Jteccomputer <chukwukachinedujohn@gmail.com>
2025-05-24 08:59:17 +0100

    Created class version of Cart

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,29 +1,34 @@
-function Cart(localStorageKey) {
-    const cart = {
-      cartItems: undefined,
+class Cart{
+    cartItems = undefined;
+    localStorageKey = undefined;
+ 
+    constructor(localStorageKey) {
+        this.localStorageKey = localStorageKey;
+        this.loadFromStorage();
+    }
 
-        loadFromStorage() {
-        this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
+    loadFromStorage() {
+            this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
 
-        if(!this.cartItems) {
-            this.cartItems = [{
-            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2,
+            if(!this.cartItems) {
+                this.cartItems = [{
+                productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 2,
 
-            deliveryOptionId: '1' 
-        }, {
-            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-            quantity: 1,
-            deliveryOptionId: '2'
-        }];
+                deliveryOptionId: '1' 
+            }, {
+                productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                quantity: 1,
+                deliveryOptionId: '2'
+            }];
         
-      };
-    },
+        };
+    }
 
     // function that save our cart to localStorage
     saveToStorage() {
-        localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems));
-    },
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    }
 
     addToCart(productId) {
     let matchingItem;
@@ -46,9 +51,9 @@ function Cart(localStorageKey) {
          }     
 
         this.saveToStorage(); 
-    },
+    }
 
-    removeFromCart(productId) {
+     removeFromCart(productId) {
         // step 1, create a new empty array
         const newCart = [];
 
@@ -64,8 +69,9 @@ function Cart(localStorageKey) {
         this.cartItems = newCart;
 
         this.saveToStorage();
-    },
+    }
 
+    
     updateDeliveryOption(productId, deliveryOptionId) {
     
         let matchingItem;
@@ -81,18 +87,20 @@ function Cart(localStorageKey) {
         this.saveToStorage();
     } 
 
-
-  };
-
-   return cart;
 }
 
-// lets use the function to generate the 2 cart objects
-const cart = Cart('cart-oop');
-const businessCart = Cart('cart-business');
+// lets use the class to generate the 2 cart objects
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
+
+/*
+cart.localStorageKey = 'cart-oop';
+businessCart.localStorageKey = 'business-cart';
 
 cart.loadFromStorage();
 businessCart.loadFromStorage();
+*/
 
 console.log(cart);
 console.log(businessCart);
+console.log(businessCart instanceof Cart);
